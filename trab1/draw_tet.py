134c1dd8be0343035513ddde9b9d890fb2c47870
gadnlino <guiavenas@gmail.com>
2019-08-28 22:14:21 -0300

    Recomecei o projeto - arquivos referentes a draw_solid.py deletados ~ mudanças em draw_tet.py

--- trab1/draw_tet.py
+++ trab1/draw_tet.py
@@ -13,11 +13,13 @@
 #     [[l_1] [l_2] .. [l_m]].
 #  m = len(mat), n = len(mat[0]), mat(i,j) = mat[i][j]
 #
-#  @author Paulo Roma
-#  @since 01/05/2014
+#  @author Guilherme Avelino
+#  @since 28
 #  @see http://www.orimosenzon.com/wiki/index.php/Python_samples
 #  @see http://mathworld.wolfram.com/RotationMatrix.html
 
+
+import numpy as np
 try:
    from tkinter import *     # python 3
 except ImportError:
@@ -27,81 +29,72 @@ from math import *
 def createZeroMat(m,n):
     """Return a matrix (m x n) filled with zeros."""
 
-    ret = [0] * m
-    for i in range(m):
-        ret[i] = [0] * n
-    return ret   
+    return np.zeros((m,n))
 
 def matMul(mat1, mat2):
     """Return mat1 x mat2 (mat1 multiplied by mat2)."""
 
-    m = len(mat1)
-    n = len(mat2[0])
-    common = len(mat2)
-   
-    ret = createZeroMat(m,n)
-    if  len(mat1[0]) == len(mat2):
-      for i in range(m):
-          for j in range(n):
-              for k in range(common):
-                  ret[i][j] += mat1[i][k] * mat2[k][j]
-    return ret
+    return mat1@mat2
 
 def matTrans(mat):
     """Return mat (n x m) transposed (m x n)."""
 
-    m = len(mat[0])
-    n = len(mat)
+    return np.transpose(mat)
 
-    ret = createZeroMat(m,n)
-    for i in range(m):
-        for j in range(n):
-            ret[i][j] = mat[j][i]
-    return ret
-
-def translate(x,y,dx,dy):
+def translate(v,dx,dy):
     """Translate vector(x,y) by (dx,dy)."""
 
-    return x+dx, y+dy
-   
+    #matriz de translação em coordenadas homogeneas
+    transl_matrix = np.array([[1.,0.,dx],[0.,1.,dy],[0.,0.,1.]])
+
+    return transl_matrix@v
+
+def hom(m):
+    return np.array([m[0],m[1],1.]) 
+
+def truncate(m):
+    return np.array([m[0],m[1]])
+
 def drawTet(tet,col):
     """Draw a tetrahedron."""
 
     w = canvas.winfo_width()/2
     h = canvas.winfo_height()/2
     canvas.delete(ALL) # delete all edges
-    nv = len(tet[0])   # number of vertices in tet (4)
+    nv = len(tet)   # number of vertices in tet (4)
 
     # draw the 6 edges of the tetrahedron
     for p1 in range(nv):
         for p2 in range(p1+1,nv):
-            canvas.create_line(translate(tet[0][p1], tet[1][p1], w, h),
-                               translate(tet[0][p2], tet[1][p2], w, h), fill = col)
+
+            print(translate(tet[p1], w, h))
+            canvas.create_line(tuple(translate(tet[p1], w, h)),
+                               tuple(translate(tet[p2], w, h)), fill = col)
 
 def init():
     """Initialize global variables."""
 
     global ROT_X, ROT_Y, ROT_Z
-    global eps, EPS, tet
+    global eps, EPS, tet, tet_faces
     global lastX, lastY, tetColor, bgColor
 
-    tet = matTrans([[0,-100,0],[-100,100,0],[100,100,0],[0,0,200]])
- 
+    tet = np.array([[0.,-100.,0.],[-100.,100.,0.],[100.,100.,0.],[0.,0.,200.]])
+    #tet_faces = np.array([[],[]])
     # counter-clockwise rotation about the X axis
-    ROT_X = lambda x: matTrans([[1,0,0],           [0,cos(x),-sin(x)], [0,sin(x),cos(x)] ])
+    ROT_X = lambda x: np.array([[1,0,0],[0,cos(x),-sin(x)], [0,sin(x),cos(x)]])
 
     # counter-clockwise rotation about the Y axis
-    ROT_Y = lambda y: matTrans([[cos(y),0,sin(y)], [0,1,0],            [-sin(y),0,cos(y)]])
+    ROT_Y = lambda y: np.array([[cos(y),0,sin(y)], [0,1,0],[-sin(y),0,cos(y)]])
 
     # counter-clockwise rotation about the Z axis
-    ROT_Z = lambda z: matTrans([[cos(z),sin(z),0], [-sin(z),cos(z),0], [0,0,1]])
+    ROT_Z = lambda z: np.array([[cos(z),sin(z),0], [-sin(z),cos(z),0], [0,0,1]])
 
     eps = lambda d: pi/300 if (d>0) else -pi/300
     EPS = lambda d: d*pi/300
 
     lastX = 0 
     lastY = 0
-    tetColor = 'blue'
+    tetColor = 'red'
     bgColor = 'white'
 
 def cbClicked(event):
@@ -120,10 +113,10 @@ def cbMottion(event):
 
     # Y coordinate is upside down
     dx = lastY - event.y 
-    tet = matMul(ROT_X(EPS(-dx)),tet)
+    tet = matMul(tet,ROT_X(EPS(-dx)))
 
     dy = lastX - event.x
-    tet = matMul(ROT_Y(EPS(dy)),tet)
+    tet = matMul(tet,ROT_Y(EPS(dy)))
 
     drawTet(tet,tetColor)
     cbClicked(event)   
@@ -132,21 +125,21 @@ def wheelUp(event):
     """Map mouse wheel up displacements to rotations about Z axis."""
 
     global tet
-    tet = matMul(ROT_Z(EPS(1)),tet)
+    tet = matMul(tet,ROT_Z(EPS(event.delta/120)))
     drawTet(tet,tetColor)
 
 def wheelDown(event):
     """Map mouse wheel down displacements to rotations about Z axis."""
 
     global tet
-    tet = matMul(ROT_Z(EPS(-1)),tet)
+    tet = matMul(tet,ROT_Z(EPS(event.delta/120)))
     drawTet(tet,tetColor)
 
 def wheel(event):
     """Map mouse wheel displacements to rotations about Z axis."""
 
     global tet
-    tet = matMul(ROT_Z(EPS(event.delta/120)),tet)
+    tet = matMul(tet,ROT_Z(EPS(event.delta/120)))
     drawTet(tet,tetColor)
 
 def resize(event):
