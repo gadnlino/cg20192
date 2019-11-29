dafdf64b1614cba0ef1c41d55e01a52390030196
Guilherme Avelino <guiavenas@gmail.com>
2019-11-29 12:10:24 -0300

    Adicionando labels nos pontos, falta mostrar

--- js/init.js
+++ js/init.js
@@ -2,11 +2,13 @@ let canvasElement;
 let menu;
 let w;
 let h;
+let font;
 let renderer;
 let camera;
 let scene;
-let selectedPoints = [];
-let curveVertices = [];
+let selectedPoints;
+let pointLabels;
+let curveVertices;
 
 function init(){
     canvasElement = document.getElementById("threejs-canvas-div");
@@ -17,22 +19,33 @@ function init(){
     renderer.setSize( w, h );
     canvasElement.appendChild(renderer.domElement);
     
+    let manager = new THREE.LoadingManager();
+    let loader = new THREE.FontLoader(manager);
+    loader.load('https://threejs.org/examples/fonts/droid/droid_serif_bold.typeface.json', function(response) {
+        font = response;
+        //console.log(response);
+    });
+
     camera = new THREE.PerspectiveCamera( 45, w / h, 1, 500 );
     camera.position.set( 0, 0, 100 );
     camera.lookAt( 0, 0, 0 );
 
     scene = new THREE.Scene();
+
+    selectedPoints = [];
+    pointLabels = [];
+    curveVertices = [];
 }
 
 function clearScene(){
     
     while(scene.children.length > 0){
-        //console.log(scene.children[]);
         scene.remove(scene.children[scene.children.length-1]);
     }
 
     curveVertices = [];
     selectedPoints = [];
+    pointLabels = [];
 
     animate();
 }
@@ -40,6 +53,18 @@ function clearScene(){
 function pushPoint(point){
     
     selectedPoints.push(point);
+
+    pushPointLabel(point, `p${selectedPoints.length-1}`);
+}
+
+function pushPointLabel([x,y,z], label){
+
+    pointLabels.push({
+        text : label,
+        position : {
+            x: x, y: y, z: z
+        }
+    });
 }
 
 function pushVertex(vertex){
@@ -86,9 +111,47 @@ function drawLines(){
     renderer.render( scene, camera );
 }
 
+function drawPointLabels(){
+
+    if(font){
+
+        pointLabels.forEach(label=>{
+            const {text, position} = label;
+            const {x,y,z} = position;
+
+            var textGeo = new THREE.TextGeometry( text, {
+
+                font: font,
+                size: 0.5,
+                height: 0.5,
+                weight:'normal',
+                bevelThickness: 2,
+                bevelSize: 1,
+                bevelEnabled: true
+            } );
+        
+            var textMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+        
+            var mesh = new THREE.Mesh( textGeo, textMaterial );
+
+            const offsetX = 2.3, offsetY = 2.3, offsetZ = 1;
+
+            mesh.position.set( x+offsetX, y+offsetY, z+offsetZ );
+        
+            scene.add( mesh );
+        });
+        
+        // Now, show what the camera sees on the screen:
+        renderer.render(scene, camera);
+
+    }
+
+}
+
 function animate(){
     
     drawPoints();
+    drawPointLabels();
     drawLines();
 }
 
